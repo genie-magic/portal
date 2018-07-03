065b9136b59f2df815489265ca37c4aafcec2fe6
HappyTinker <flashingtinker@gmail.com>
2018-07-03 09:39:38 -0400

    fix mailApi.js request position

--- src/modules/mail/mailApi.js
+++ src/modules/mail/mailApi.js
@@ -1,15 +1,11 @@
 import { wrapRequest } from '../utils';
 
 const postMailRequest = wrapRequest(async mailData =>
-  fetch('http://www.ericleesite.com/api/mail', {
+  fetch('http://localhost:5000/api/mail', {
     body: JSON.stringify(mailData), // must match 'Content-Type' header
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
-    },
-    mode: 'cors',
-    cors: {
-      origin: ['*']
     },
     method: 'POST' // *GET, POST, PUT, DELETE, etc.
   })

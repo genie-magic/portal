afaf9b6ece30fc88996088320fd3a34a9ed69cd6
HappyTinker <flashingtinker@gmail.com>
2018-07-03 07:01:16 -0400

    fix mail api

--- src/modules/mail/mailApi.js
+++ src/modules/mail/mailApi.js
@@ -1,7 +1,7 @@
 import { wrapRequest } from '../utils';
 
 const postMailRequest = wrapRequest(async mailData =>
-  fetch('http://localhost:5000/api/mail', {
+  fetch('/api/mail', {
     body: JSON.stringify(mailData), // must match 'Content-Type' header
     headers: {
       'content-type': 'application/json'

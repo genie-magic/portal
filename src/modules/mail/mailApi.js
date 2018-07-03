9d44284154bef625fee1bfa88d06fbbc123576ac
root <root@ip-172-31-24-97.ec2.internal>
2018-07-03 14:57:34 +0000

    fix contact form

--- src/modules/mail/mailApi.js
+++ src/modules/mail/mailApi.js
@@ -1,7 +1,7 @@
 import { wrapRequest } from '../utils';
 
 const postMailRequest = wrapRequest(async mailData =>
-  fetch('http://localhost:5000/api/mail', {
+  fetch('http://www.ericleesite.com:5000/api/mail', {
     body: JSON.stringify(mailData), // must match 'Content-Type' header
     headers: {
       Accept: 'application/json',
